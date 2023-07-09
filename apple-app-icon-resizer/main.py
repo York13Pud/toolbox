b5b49806fcf50766dc3c1762f5c0abc4540786b7
York13Pud <neilallwood@icloud.com>
2023-07-09 15:47:46 +0100

    Updated code

--- apple-app-icon-resizer/main.py
+++ apple-app-icon-resizer/main.py
@@ -1,10 +1,16 @@
 # --- Import the required libraries:
 import json
 import os
+
+from pathlib import Path
 from PIL import Image
 
 
 # --- Define constants and variables:
+APP_DIR = Path(__file__).resolve().parent
+EXPORT_DIR = ""
+# print(WORKING_DIR)
+
 file_name = "app-icon"
 file_extensions = ["jpg", "jpeg", "png"]
 file_found = False
@@ -25,7 +31,10 @@ print(app_icon_image_sizes)
 
 
 # --- Check that the file is the right size (1024 x 1024):
+image = Image.open("test.png")
+print(image.size[0], image.size[0])
 
 
 # --- Resize the file and save it for each one that is in the JSON file:
-
+# image.thumbnail(size = (500,500))
+# image.save("test1.png", "png")
